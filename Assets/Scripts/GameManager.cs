644b2708ca904f4bbd79528ba31de8597a859204
Raúl Francés <raulfrancesm1997@gmail.com>
2024-07-26 12:48:02 +0200

    Proyecto inicializado

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadScene(int id)
+    {
+        SceneManager.LoadScene(id);
+    }
+
+}
