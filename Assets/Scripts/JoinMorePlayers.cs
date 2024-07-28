eb177a0f42eeacd50f125c4d40d71f5ffc2532e7
Raúl Francés <raulfrancesm1997@gmail.com>
2024-07-28 11:41:54 +0200

    bugs

--- Assets/Scripts/JoinMorePlayers.cs
+++ Assets/Scripts/JoinMorePlayers.cs
@@ -16,13 +16,14 @@ public class JoinMorePlayers : MonoBehaviour
         if (GameManager.instance.playerList.Contains(player.GetComponent<Player>())) { return; }
         //player.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite = character[GameManager.instance.playerList.Count];
         player.GetComponent<Player>().character = (Character)GameManager.instance.spawnId - 1;
-        Invoke(nameof(KeepDoingStuff), 0.1f);
+        StartCoroutine(KeepDoingStuff());
     }
 
-    public void KeepDoingStuff()
+    public IEnumerator KeepDoingStuff()
     {
+        yield return null;
         player.GetComponent<Player>().SetColor(GameManager.instance.GetColorFor(player.GetComponent<Player>().character));
-        player.transform.DOMove(GameManager.instance.spawns[(int)player.GetComponent<Player>().character].position, 0.001f).Play();
+        player.transform.DOMove(GameManager.instance.spawns[(int)player.GetComponent<Player>().character].position, 0.001f).SetUpdate(true).Play();
         GameManager.instance.spawnId++;
     }
 }
